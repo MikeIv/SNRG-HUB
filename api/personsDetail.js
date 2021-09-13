c33acc44b990bee5a689c94e445815e454f44b70
Bochkarev Ivan <ivanx86@gmail.com>
2021-09-13 21:01:16 +0600

    OGM-620 Метод получения конкретной персоны по фильтру

---
+++ api/personsDetail.js
@@ -0,0 +1,10 @@
+import { axiosCreate } from '~/axios/axios'
+
+const axios = axiosCreate()
+
+// Метод получения конкретной персоны по фильтру
+
+export default async function getPersonsDetail(requestData) {
+  const response = await axios.post('api/v1/persons/detail', requestData)
+  return response.data.data
+}
