7fc9cf172b522385b394e25146882d00c98b87b0
AShumenkova@synergy.ru <AShumenkova@synergy.ru>
2021-09-13 17:33:27 +0300

    Метод получения персон по фильтру: persons/list

---
+++ api/personsList.js
@@ -0,0 +1,11 @@
+import { axiosCreate } from '~/axios/axios'
+
+const axios = axiosCreate()
+
+//Метод получения персон по фильтру
+//
+
+export default async function getPersonsList(requestData) {
+    const response = await axios.post('api/v1/persons/list', requestData)
+    return response.data
+}
