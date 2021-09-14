e9301b23e6b82fc66c5d1d752445f2a274aeefc7
lshirokova <lshirokova@synergy.ru>
2021-09-14 17:05:02 +0400

    feat(api): feat(api): метод получения квизов по фильтру

--- api/quizzesList.js
+++ api/quizzesList.js
@@ -1,10 +1,10 @@
-import { axiosCreate } from '~/axios/axios'
+import { axiosCreate } from '~/axios/axios';
 
-const axios = axiosCreate()
+const axios = axiosCreate();
 
 // Метод получения квизов по фильтру
 
 export default async function getQuizzesList(requestData) {
-  const response = await axios.post('api/v1/quizzes/list', requestData)
-  return response.data.data
+  const response = await axios.post('api/v1/quizzes/list', requestData);
+  return response.data.data;
 }
