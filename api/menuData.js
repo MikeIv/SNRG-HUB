970715affdba6754905cd0286b9cece44d992e71
lshirokova <lshirokova@synergy.ru>
2021-09-14 17:49:04 +0400

    feat(api): feat(api): oGM-622 Метод получения меню: menu

--- api/menuData.js
+++ api/menuData.js
@@ -1,10 +1,10 @@
-import { axiosCreate } from '~/axios/axios'
+import { axiosCreate } from '~/axios/axios';
 
-const axios = axiosCreate()
+const axios = axiosCreate();
 
 // Метод получения меню
 
 export default async function getMenuData(requestData) {
-  const response = await axios.post('api/v1/menu', requestData)
-  return response.data.data
+  const response = await axios.post('api/v1/menu', requestData);
+  return response.data.data;
 }
