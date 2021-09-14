20acad963399eea6659a1ee0f2933e82dc4ad47d
Igor Shavyrin <straiker_b12@mail.ru>
2021-09-14 11:38:24 +0300

    OGM-628

---
+++ api/organizationsSectionsDetail.js
@@ -0,0 +1,10 @@
+import { axiosCreate } from '~/axios/axios'
+
+const axios = axiosCreate()
+
+// Метод получения конкретной секции организации по фильтру
+
+export default async function getOrganizationsSectionsDetail(requestData) {
+  const response = await axios.post('api/v1/organizations/sections/detail', requestData)
+  return response.data.data
+}
