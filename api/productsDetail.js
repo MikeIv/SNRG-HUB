f43b4767f98999214f73aecae64cbe65dc17d468
AShumenkova@synergy.ru <AShumenkova@synergy.ru>
2021-09-14 11:15:59 +0300

    t

--- api/productsDetail.js
+++ api/productsDetail.js
@@ -3,7 +3,6 @@ import { axiosCreate } from '~/axios/axios'
 const axios = axiosCreate()
 
 // Метод получения конкретного продукта по фильтру
-
 export default async function getProductsDetails(requestData) {
   const response = await axios.post('api/v1/products/detail', requestData)
   return response.data.data
