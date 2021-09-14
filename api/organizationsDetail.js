25ed60b20146e3d1857252fe4b39839d82d341cc
Иванов Михаил Юрьевич <MIUIvanov@synergy.ru>
2021-09-14 13:34:51 +0300

    config OrganizationsDetail

---
+++ api/organizationsDetail.js
@@ -0,0 +1,10 @@
+import { axiosCreate } from '~/axios/axios'
+
+const axios = axiosCreate()
+
+// Метод получения конкретной организации по фильтру
+
+export default async function getOrganizationsDetail(requestData) {
+  const response = await axios.post('api/v1/organizations/detail', requestData)
+  return response.data.data
+}
