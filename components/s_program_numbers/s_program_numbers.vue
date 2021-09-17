f94396e3959863dfb0440d3f6718633defd1f3b2
lshirokova <lshirokova@synergy.ru>
2021-09-17 15:57:51 +0400

    build(components): oGM-713 s-program-numbers

---
+++ components/s_program_numbers/s_program_numbers.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="s-program-numbers__wrapper">
+    <AFactoid
+      v-for="factoid in factoids"
+      :key="factoid.id"
+      :type="factoid.type"
+      :title="factoid.title"
+      :number="factoid.number"
+    />
+  </div>
+</template>
+
+<script>
+import { AFactoid } from '@cwespb/synergyui';
+import '@cwespb/synergyui/lib/synergyui.css';
+import './s_program_numbers.scss';
+
+export default {
+  name: 'SProgramNumbers',
+
+  components: {
+    AFactoid,
+  },
+
+  data() {
+    return {
+      factoids: [
+        {
+          lineNumber: 'от 120 000 ₽',
+          title: 'в год',
+          type: 'line',
+        },
+        {
+          lineNumber: '20',
+          title: 'бюджетных мест',
+          type: 'line',
+        },
+        {
+          lineNumber: '60',
+          title: 'платных мест',
+          type: 'line',
+        },
+      ],
+    };
+  },
+};
+</script>
