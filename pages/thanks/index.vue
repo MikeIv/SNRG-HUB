4e4f947d7c21f05a4efddd01151ae14ada2e8e4f
SDuysakaev@synergy.ru <SDuysakaev@synergy.ru>
2021-09-19 17:01:14 +0300

    feat(components): страницы thanks и 404
    Добавлены страницы и компоненты thanks и 404,
    
    НОМЕР ЗАДАЧИ: OGM-691

---
+++ pages/thanks/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="thanks__wrapper">
+    <!-- <footer class="header"></footer> -->
+    <thanks></thanks>
+    <!-- <footer class="footer"></footer> -->
+  </div>
+</template>
+
+<script>
+import thanks from '~/components/thanks/thanks';
+
+export default {
+  components: { thanks },
+};
+</script>
+
+<style lang="scss">
+/* .footer {
+  width: 100%;
+  min-height: 204px;
+  background-color: #dcf3ff;
+}
+.header {
+  width: 100%;
+  min-height: 172px;
+  background-color: #dcf3ff;
+} */
+</style>
