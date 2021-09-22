ee0b2ec12384d270b29be2354bd1a56b1578303c
Bochkarev Ivan <ivanx86@gmail.com>
2021-09-22 21:02:01 +0600

    fix(components): исправление наложения изображения в секции
    НОМЕР ЗАДАЧИ: OGM-713
    
    НОМЕР ЗАДАЧИ: OGM-713

--- components/s_program_info/s_program_info.vue
+++ components/s_program_info/s_program_info.vue
@@ -58,7 +58,7 @@ export default {
         breakpoints: {
           576: {
             slidesPerView: 'auto',
-            spaceBetween: 30,
+            spaceBetween: 12,
           },
         },
       },
