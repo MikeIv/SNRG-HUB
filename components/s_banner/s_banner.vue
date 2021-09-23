63ab4fab5f95081c89ad7b386f1c605f938865eb
Иванов Михаил Юрьевич <MIUIvanov@synergy.ru>
2021-09-23 17:32:17 +0300

    feat(components): подключение компонента Баннер
    Подключен компонент Баннер
    
    НОМЕР ЗАДАЧИ: OGM-687

--- components/s_banner/s_banner.vue
+++ components/s_banner/s_banner.vue
@@ -1,18 +1,15 @@
 <template>
   <section class="l-wide">
-    <h2 class="s_banner__h2 a-font_h5">{{ title }}</h2>
-    <h2 class="s_banner__h2 a-font_h5">{{ methods }}</h2>
-    <nuxt-link :to="redirectUrl" class="s_banner__link">
+    <nuxt-link :to="`${baseURL}/${bannersDetail.link}`" class="s_banner__link">
       <m-banner
         class="s_banner"
-        type="narrow"
-        bgColor="secondary"
-        leftCol="active"
-        topTxt="topTxt"
-        titleTxt="titleTxt"
-        secondTxt="secondTxt"
-        textCol="active"
-        color="custom"
+        :type="bannersDetail.banner_type"
+        :bgColor="bannersDetail.color_bg"
+        :topTxt="bannersDetail.description"
+        :titleTxt="bannersDetail.name"
+        :secondTxt="bannersDetail.name_second"
+        :color="bannersDetail.color_text"
+        :ImgSrc="`${baseURL}/${bannersDetail.image}`"
       />
     </nuxt-link>
   </section>
@@ -32,42 +29,20 @@ export default {
 
   data() {
     return {
-      bannersDetail: [],
+      bannersDetail: {},
       baseURL: process.env.NUXT_ENV_S3BACKET,
       windowWidth: 0,
       redirectUrl: '#',
     };
   },
-  props: ['methods', 'title', 'view_type'],
-  methods: {
-    getWindowWidth() {
-      this.windowWidth = document.documentElement.clientWidth;
-    },
-  },
+  props: ['methods', 'title'],
+  methods: {},
   async fetch() {
-    const expandedBanners = this.methods.data;
-    this.bannersDetail = await getBannersDetail(expandedBanners);
+    let [expandedMethod] = this.methods;
+    expandedMethod = { ...expandedMethod.data };
+    this.bannersDetail = await getBannersDetail(expandedMethod);
   },
-  mounted() {
-    this.$nextTick(function () {
-      window.addEventListener('resize', this.getWindowWidth);
-      // Init
-      this.getWindowWidth();
-    });
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize', this.getWindowWidth);
-  },
-  computed: {
-    cardsSet() {
-      let cardsList = 0;
-      if (this.windowWidth > 991) {
-        cardsList = 6;
-      } else if (this.windowWidth < 992) {
-        cardsList = 4;
-      }
-      return cardsList;
-    },
-  },
+  mounted() {},
+  computed: {},
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">{{ pageInfo.name }}</h1>
-    </div>
-
-    <br />
-    <h2>DATA FROM EXAMPLE:</h2>
-    <example />
-    <br />
-    <h2>PAGE INFO:</h2>
-    <!--    <component :is="section" v-for="section in sections" :key="section"></component>-->
+  <div>
     <component
       :is="key"
       v-for="{ key, methods, title, id } in pageInfo.components"
@@ -24,37 +14,8 @@
 </template>
 
 <script>
-// import Example from '~/components/Example';
-import banner_main_page from '~/components/banner_main_page/banner_main_page';
-
 export default {
-  // components: { Example },
-
   middleware: 'getPageInfo',
-
-  components: { banner_main_page },
-
-  asyncData() {
-    // Массив имен для component :is будет формироваться от бэка
-    // На данный момент там имена неправильные, поэтому хардкод
-    // А так мы их будем брать из pageInfo.components и у объекта ключ "key"
-    const sections = [
-      's-main-topics',
-      's-main-statistics',
-      's-main-programs',
-      's-main-search',
-      's-main-university',
-      's-main-success',
-      's-main-banners',
-      's-main-banners-narrow',
-      's-banner',
-      's-partners',
-    ];
-
-    return {
-      sections,
-    };
-  },
 
   computed: {
     pageInfo() {
