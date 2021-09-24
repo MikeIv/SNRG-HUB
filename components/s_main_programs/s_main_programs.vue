f2203a7ea93343aa30767c06f7ad0beff4aadc4a
Иванов Михаил Юрьевич <MIUIvanov@synergy.ru>
2021-09-24 09:27:51 +0300

    feat(components): компонент viewtype 2
    Добавлен вариант отображения с баннером
    
    НОМЕР ЗАДАЧИ: OGM-686

--- components/s_main_programs/s_main_programs.vue
+++ components/s_main_programs/s_main_programs.vue
@@ -1,25 +1,31 @@
 <template>
-  <section class="l-wide">
+  <section class="l-wide s_main_programs">
     <h2 class="s_main_programs__h2 a-font_h5">{{ title }}</h2>
-    <div class="s_main_programs__cards">
-      <template v-for="(product, index) in productsList">
-        <nuxt-link
-          to="/#"
-          class="s_main_programs__wrapper"
-          v-if="productsList && productsList.length > 0 && index < cardsSet"
-          :key="product.id"
-        >
-          <m-card
-            :title="product.name"
-            :bottomText="product.included.organization.name"
-            :name="product.name"
-            :description="product.included.levels[0].name"
-            :iconSrc="`${baseURL}/${product.included.organization.logo}`"
-            :verticalImgSrc="`${baseURL}/${product.digital_image}`"
-            type="program"
-            @click="$router.push({ name: 'product', params: { id: product.id } })"
-          />
-        </nuxt-link>
+    <div class="s_main_programs__row">
+      <div class="s_main_programs__cards">
+        <template v-for="(product, index) in productsList">
+          <nuxt-link
+            to="/#"
+            class="s_main_programs__wrapper"
+            v-if="productsList && productsList.length > 0 && index < cardsSet"
+            :key="product.id"
+          >
+            <m-card
+              :title="product.name"
+              :bottomText="product.included.organization.name"
+              :name="product.name"
+              :description="product.included.levels[0].name"
+              :iconSrc="`${baseURL}/${product.included.organization.logo}`"
+              :verticalImgSrc="`${baseURL}/${product.digital_image}`"
+              type="program"
+              @click="$router.push({ name: 'product', params: { id: product.id } })"
+            />
+          </nuxt-link>
+        </template>
+      </div>
+
+      <template v-if="Object.keys(bannersDetail).length">
+        <s-banner />
       </template>
     </div>
 
@@ -31,6 +37,7 @@
 
 <script>
 import { MCard, AButton } from '@cwespb/synergyui';
+import SBanner from '~/components/s_banner/s_banner';
 import getProductsList from '~/api/products_list';
 import getBannersDetail from '~/api/bannersDetail';
 import './s_main_programs.scss';
@@ -41,12 +48,13 @@ export default {
   components: {
     MCard,
     AButton,
+    SBanner,
   },
 
   data() {
     return {
       productsList: [],
-      bannersDetail: [],
+      bannersDetail: {},
       baseURL: process.env.NUXT_ENV_S3BACKET,
       windowWidth: 0,
       redirectUrl: '#',
@@ -56,6 +64,13 @@ export default {
   methods: {
     getWindowWidth() {
       this.windowWidth = document.documentElement.clientWidth;
+    },
+    addBanner() {
+      if (this.inputs.length === 0) {
+        this.inputs.push({
+          rangField: '',
+        });
+      }
     },
   },
   async fetch() {
