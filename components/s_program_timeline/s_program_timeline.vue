1f134d0377aa1343219526addde40c62a1f7e9b9
Bochkarev Ivan <ivanx86@gmail.com>
2021-09-26 11:38:36 +0600

    feat(components): интеграция s-program-timeline
    НОМЕР ЗАДАЧИ: OGM-764

--- components/s_program_timeline/s_program_timeline.vue
+++ components/s_program_timeline/s_program_timeline.vue
@@ -2,7 +2,7 @@
   <section class="s-program-timeline">
     <div class="s-program-timeline__header">
       <h2 class="s-program-timeline__title a-font_h2" v-html="title"></h2>
-      <div class="s-program-timeline__factoids">
+      <div class="s-program-timeline__factoids" v-if="factoids">
         <AFactoid
           v-for="factoid in factoids"
           :key="factoid.id"
@@ -14,7 +14,10 @@
     </div>
     <div class="s-program-timeline__swiper">
       <swiper :options="swiperOptionA">
-        <swiper-slide v-for="item in items" :key="item.id" class="s-program-timeline__slide m-card-landing">
+        <swiper-slide
+          v-for="item in programTimelineList"
+          :key="item.id"
+          class="s-program-timeline__slide m-card-landing">
           <MCardLanding :title="item.title" :text="item.text" :image="item.image" />
         </swiper-slide>
       </swiper>
@@ -28,6 +31,8 @@ import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 import { AFactoid, MCardLanding } from '@cwespb/synergyui';
 import './s_program_timeline.scss';
 
+import getEntitiesSectionsDetail from '~/api/entitiesSectionsDetail';
+
 export default {
   name: 's_program_timeline',
 
@@ -40,6 +45,7 @@ export default {
 
   data() {
     return {
+      programTimelineList: [],
       baseUrl: process.env.NUXT_ENV_S3BACKET,
       swiperOption: {
         grabCursor: true,
@@ -57,7 +63,6 @@ export default {
         grabCursor: true,
         slidesPerView: 'auto',
         spaceBetween: 20,
-        loop: true,
         resistance: true,
         resistanceRatio: 0,
         breakpoints: {
@@ -67,50 +72,32 @@ export default {
         },
       },
 
-      title: 'Как проходит <span>обучение</span>',
-      factoids: [
-        {
-          title: '6 месяцев',
-          subtitle: 'Длительность',
-          type: 'default',
-        },
-        {
-          title: 'командный кейс',
-          subtitle: 'Дипломный проект',
-          type: 'default',
-        },
-      ],
-      items: [
-        {
-          title: 'Изучаете тему',
-          text: 'Смотрите обучающие видео и задаёте вопросы',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          title: 'Изучаете тему',
-          text: 'Смотрите обучающие видео и задаёте вопросы',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          title: 'Изучаете тему',
-          text: 'Смотрите обучающие видео и задаёте вопросы',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          title: 'Изучаете тему',
-          text: 'Смотрите обучающие видео и задаёте вопросы',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-      ],
+      // factoids: [
+      //   {
+      //     title: '6 месяцев',
+      //     subtitle: 'Длительность',
+      //     type: 'default',
+      //   },
+      //   {
+      //     title: 'командный кейс',
+      //     subtitle: 'Дипломный проект',
+      //     type: 'default',
+      //   },
+      // ],
+
     };
+  },
+
+  props: ['methods', 'title'],
+  async fetch() {
+    const expandedMethod = this.methods[0].data;
+    const preData = await getEntitiesSectionsDetail(expandedMethod);
+    this.programTimelineList = preData.json.items.data.map((item) => ({
+      title: item.title.value,
+      text: item.description.value,
+      // Todo: Проверка на пустоту preview_image - сейчас значение пустое
+      // image: item.preview_image.value ? item.preview_image.value : '',
+    }));
   },
 };
 </script>
