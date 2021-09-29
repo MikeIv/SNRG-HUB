d3cfc4c36820f6eef12b013effe76a422c3d3b0f
Иванов Михаил Юрьевич <MIUIvanov@synergy.ru>
2021-09-29 19:07:23 +0300

    feat(components): s-program-teachers
    Подключение даты
    
    НОМЕР ЗАДАЧИ: OGM-764

--- components/s_program_teachers/s_program_teachers.vue
+++ components/s_program_teachers/s_program_teachers.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="s-program-teachers">
-    <h2 class="s-program-teachers__title a-font_h2" v-html="title"></h2>
-    <div class="s-program-teachers__items s-program-teachers__items_horisontal" v-if="items.length < 4">
+    <h2 class="s-program-teachers__title a-font_h2">{{ title }}</h2>
+    <div class="s-program-teachers__items s-program-teachers__items_horisontal" v-if="programTeachers.length < 4">
       <MCardSpeaker
-        v-for="item in items"
+        v-for="item in programTeachers"
         :key="item.id"
         :name="item.name"
         :title="item.title"
@@ -13,7 +13,11 @@
     </div>
     <div class="s-program-teachers__items s-program-teachers__items_vertical" v-else>
       <swiper :options="swiperOptionA">
-        <swiper-slide v-for="item in items" :key="item.id" class="s-program-teachers__slide m-card-landing-vertical">
+        <swiper-slide
+          v-for="item in programTeachers"
+          :key="item.id"
+          class="s-program-teachers__slide m-card-landing-vertical"
+        >
           <MCardLanding :title="item.title" :text="item.description" :image="item.image" />
         </swiper-slide>
       </swiper>
@@ -24,7 +28,10 @@
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 import { MCardSpeaker, MCardLanding } from '@cwespb/synergyui';
+import getProductsDetails from '~/api/productsDetail';
+
 import './s_program_teachers.scss';
+import getEntitiesSectionsDetail from '~/api/entitiesSectionsDetail';
 
 export default {
   name: 's_program_teachers',
@@ -38,6 +45,7 @@ export default {
 
   data() {
     return {
+      programTeachers: [],
       swiperOptionA: {
         grabCursor: true,
         slidesPerView: 'auto',
@@ -45,60 +53,18 @@ export default {
         resistance: true,
         resistanceRatio: 0,
       },
-      items: [
-        {
-          name: 'Андрей Борю',
-          title: 'Основатель Академии Infostorm',
-          description:
-            // eslint-disable-next-line max-len
-            'В разработке более 12 лет, изначально программировал на php, потом освоил Golang, java, python. Последние несколько лет активно занимается devops. Работал в центральном вьетнамском офисе Лазады — крупнейшем азиатском маркетплейсе, и в офисе финтех-проекта в Литве',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          name: 'Андрей Борю',
-          title: 'Основатель Академии Infostorm',
-          description:
-            // eslint-disable-next-line max-len
-            'В разработке более 12 лет, изначально программировал на php, потом освоил Golang, java, python. Последние несколько лет активно занимается devops. Работал в центральном вьетнамском офисе Лазады — крупнейшем азиатском маркетплейсе, и в офисе финтех-проекта в Литве',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          name: 'Андрей Борю',
-          title: 'Основатель Академии Infostorm',
-          description:
-            // eslint-disable-next-line max-len
-            'В разработке более 12 лет, изначально программировал на php, потом освоил Golang, java, python. Последние несколько лет активно занимается devops. Работал в центральном вьетнамском офисе Лазады — крупнейшем азиатском маркетплейсе, и в офисе финтех-проекта в Литве',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          name: 'Андрей Борю',
-          title: 'Основатель Академии Infostorm',
-          description:
-            // eslint-disable-next-line max-len
-            'В разработке более 12 лет, изначально программировал на php, потом освоил Golang, java, python. Последние несколько лет активно занимается devops. Работал в центральном вьетнамском офисе Лазады — крупнейшем азиатском маркетплейсе, и в офисе финтех-проекта в Литве',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-        {
-          name: 'Андрей Борю',
-          title: 'Основатель Академии Infostorm',
-          description:
-            // eslint-disable-next-line max-len
-            'В разработке более 12 лет, изначально программировал на php, потом освоил Golang, java, python. Последние несколько лет активно занимается devops. Работал в центральном вьетнамском офисе Лазады — крупнейшем азиатском маркетплейсе, и в офисе финтех-проекта в Литве',
-          image:
-            // eslint-disable-next-line max-len
-            'https://images.unsplash.com/photo-1528287942171-fbe365d1d9ac?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&w=1200&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
-        },
-      ],
-      title: '<span>Преподаватели</span> курса',
     };
+  },
+
+  props: ['methods', 'title'],
+  async fetch() {
+    const expandedMethod = { filter: {} };
+    expandedMethod.include = ['persons'];
+    expandedMethod.filter.slug = this.$route.params.slug;
+    const preData = await getProductsDetails(expandedMethod);
+    console.log('@@@@', preData);
+    this.programTeachers = preData.data.included.persons;
+    console.log('@@', this.programTeachers);
   },
 };
 </script>
