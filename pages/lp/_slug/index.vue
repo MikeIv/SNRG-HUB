f717dd99c65a95d01e1e4f3095126aadc4428431
Автушенко Андрей Николаевич <AAvtushenko@synergy.ru>
2021-10-15 16:16:18 +0300

    feat(components): added all needed styles to page

--- pages/lp/_slug/index.vue
+++ pages/lp/_slug/index.vue
@@ -5,7 +5,7 @@
         <nuxt-link to="/" class="s-header__logo-link">
           <a-logo class="s-header-lp__logo" type="standart" :link="logoURL"></a-logo>
         </nuxt-link>
-        <h3 class="a-font_XL">Образовательная платформа</h3>
+        <h3 class="a-font_XL s-header-lp__left-text">Образовательная платформа</h3>
       </div>
       <div class="s-header-lp__right">
         <i
@@ -179,6 +179,10 @@ export default {
 
     @media screen and (max-width: 767px) {
       display: block;
+    }
+
+    &-text {
+      white-space: nowrap;
     }
   }
 
