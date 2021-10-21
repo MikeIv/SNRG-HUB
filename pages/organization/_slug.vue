f8614ca4cfd12c13068c217f166303b4b61a0058
Борцов Дмитрий <job.cwe@yandex.ru>
2021-10-21 23:33:21 +0300

    fix(components): fix meta, catalog, upd kit

--- pages/organization/_slug.vue
+++ pages/organization/_slug.vue
@@ -36,8 +36,34 @@ export default {
       title: this.pageMeta?.title,
       meta: [
         {
-          keywords: this.pageMeta?.keywords,
-          description: this.pageMeta?.description,
+          hid: 'keywords',
+          name: 'keywords',
+          content: this.pageMeta?.keywords,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.pageMeta?.description,
+        },
+        {
+          hid: 'og:type',
+          name: 'og:type',
+          content: 'website',
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.pageMeta?.title,
+        },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: 'Synergyeducation',
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.pageMeta?.description,
         },
       ],
       bodyAttrs: {
