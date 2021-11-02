c79fcbe77bcf27caed8aec8d72aacea0b220c21a
Andrey Avtushenko <AAvtushenko@synergy.ru>
2021-11-02 14:28:18 +0300

    feat(components): fix redirect and comment prop in formData
    also fixed header, added buttons on cards and new popup

--- pages/edu-platform/_slug/thanks.vue
+++ pages/edu-platform/_slug/thanks.vue
@@ -41,7 +41,7 @@ export default {
 
   methods: {
     goBack() {
-      this.$router.push({ name: 'edu-platform-slug' });
+      this.$router.push({ name: 'edu-platform-slug', params: this.$route.params });
     },
   },
 };
