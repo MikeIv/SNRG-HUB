b8ba33ac54560bed259de14741439be38d99e323
ogairodion <ogairodion@gmail.com>
2021-11-12 00:50:05 +0300

    fix(components): fix(components): правки ошибки 404
    НОМЕР ЗАДАЧИ: hotfix_popup_school

--- components/product_school/s_program_studies/s_program_studies.vue
+++ components/product_school/s_program_studies/s_program_studies.vue
@@ -34,41 +34,22 @@
         :submitDisabled="!validFlag"
         @submit-disabled="validFlag = $event"
         typeCtrl="checkbox"
-        ref="form"
+        ref="popupform-ref"
         @click="sendForm"
       >
         <template v-slot:inputs>
-          <AInput
-            class="m-form__input"
-            @input="
-              handlerSave();
-              validFormData();
-            "
-            v-model="fieldsData.name"
-            placeholder="Имя"
-          />
+          <AInput class="m-form__input" @input="validFormData()" v-model="fieldsData.name" placeholder="Имя" />
 
           <AInput
             type="phone"
             class="m-form__input"
             @validate="validatePhone"
             v-model="fieldsData.phone"
-            @input="
-              handlerSave();
-              validFormData();
-            "
+            @input="validFormData()"
             placeholder="Телефон"
           />
 
-          <AInput
-            class="m-form__input"
-            @input="
-              handlerSave();
-              validFormData();
-            "
-            v-model="fieldsData.email"
-            placeholder="Почта"
-          />
+          <AInput class="m-form__input" @input="validFormData()" v-model="fieldsData.email" placeholder="Почта" />
         </template>
       </MForm>
     </APopup>
@@ -119,7 +100,6 @@ export default {
       validFlag: false,
       fieldsData: {
         name: '',
-        phone: '',
         email: '',
       },
       popupOptions: {
@@ -136,12 +116,13 @@ export default {
   },
 
   mounted() {
-    this.$emit('form-ref', this.$refs.form);
+    this.$emit('popupform-ref', this.$refs.form);
 
-    const loadDataForm = this.$lander.storage.load('programform');
+    const loadDataForm = this.$lander.storage.load('popupform');
 
     if (loadDataForm) this.fieldsData = loadDataForm;
-    this.validFlag = this.$lander.valid(loadDataForm);
+
+    this.validFormData();
 
     this.title = this.dataObject.title;
     this.subtitle = this.dataObject.subtitle;
@@ -157,15 +138,14 @@ export default {
         .send(this.fieldsData, {}, this.$route.name === 'edu-platform-slug' ? this.$route.path : undefined)
         .then(() => {});
     },
-    handlerSave() {
-      this.$lander.storage.save('programform', this.fieldsData);
-    },
     validatePhone(value) {
       this.validPhone = value.valid;
     },
     validFormData() {
       const dataForm = [{ value: this.fieldsData.name }, { value: this.fieldsData.email, type: 'email' }];
       this.validFlag = this.$lander.valid(dataForm) && this.validPhone;
+
+      this.$lander.storage.save('form-reg', this.fieldsData);
     },
   },
 };
