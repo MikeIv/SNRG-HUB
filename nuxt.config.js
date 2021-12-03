b3e0e8884931d4cbe679f399bbea1e1a0d27be4d
AAvtushenko <AAvtushenko@synergy.ru>
2021-12-03 17:14:22 +0300

    refactor(components): optimize time to render lp

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,14 @@ export default {
       { rel: 'preconnect', href: 'https://mp.synergy.ru' },
       { rel: 'preconnect', href: 'https://connect.facebook.net' },
     ],
+    script: [
+      {
+        type: 'text/javascript',
+        // eslint-disable-next-line quotes
+        innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-WPTVBRG");`,
+      },
+    ],
+    __dangerouslyDisableSanitizers: ['script'],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
