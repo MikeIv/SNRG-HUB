806a004afd1643da77117d6e23af34af5f89ef1f
AShumenkova <AShumenkova@synergy.ru>
2022-02-17 13:33:39 +0300

    fix(components): удаление класса
    удаление класса
    
    НОМЕР ЗАДАЧИ: OGM-1812

--- components/popup_location/popup_location.vue
+++ components/popup_location/popup_location.vue
@@ -100,7 +100,6 @@ export default {
         aPopup.classList.add('active');
       } else {
         document.documentElement.classList.remove('cityPopupFocused');
-        aPopup.classList.remove('active');
       }
     },
 
