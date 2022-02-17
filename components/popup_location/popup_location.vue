4bcfc56b53ba3a18aced39f042b4fe075965a3f0
AShumenkova <AShumenkova@synergy.ru>
2022-02-17 13:16:24 +0300

    feat(components): удаление добавление класса
    удаление добавление класса
    
    НОМЕР ЗАДАЧИ: OGM-1812

--- components/popup_location/popup_location.vue
+++ components/popup_location/popup_location.vue
@@ -100,6 +100,7 @@ export default {
         aPopup.classList.add('active');
       } else {
         document.documentElement.classList.remove('cityPopupFocused');
+        aPopup.classList.remove('active');
       }
     },
 
