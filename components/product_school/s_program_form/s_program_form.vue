68d74c99d9268ea44030fe48ae9281153a5fcada
AShumenkova <AShumenkova@synergy.ru>
2022-02-18 16:59:59 +0300

    fix(components): убрать красную окантовку
    Убрать красную окантовку
    
    НОМЕР ЗАДАЧИ: OGM-1838

--- components/product_school/s_program_form/s_program_form.vue
+++ components/product_school/s_program_form/s_program_form.vue
@@ -14,32 +14,25 @@
       <template v-slot:inputs>
         <a-input
           class="m-form__input"
-          :class="{ 'error-name': !validName }"
-          @input="
-            handlerSave();
-            validFormData();
-          "
+          :class="{ 'error-name': !nameErrorFlag }"
+          @input="validFormData"
           v-model="fieldsData.name"
           placeholder="Имя"
         />
         <vue-tel-input
           class="m-form__input"
-          :class="{ error: !validPhone }"
+          :class="{ error: !phoneErrorFlag }"
           v-bind="vueTelOpts"
           type="phone"
           placeholder="Телефон"
-          @validate="validFormData"
           v-model="fieldsData.phone"
           @input="validatePhone"
         >
         </vue-tel-input>
         <a-input
           class="m-form__input"
-          :class="{ 'error-mail': !validFlag }"
-          @input="
-            handlerSave();
-            validFormData();
-          "
+          :class="{ 'error-mail': !emailErrorFlag }"
+          @input="validFormData"
           v-model="fieldsData.email"
           placeholder="Почта"
         />
@@ -85,9 +78,18 @@ export default {
     typeCtrl: 'checkbox',
     typeBtn: 'checkbox',
     checked: true,
-    validFlag: false,
+    validFlag: true,
     validName: false,
+    nameErrorFlag: true,
+    emailErrorFlag: true,
+    phoneErrorFlag: true,
 
+    fieldsData: {
+      name: '',
+      phone: '',
+      email: '',
+    },
+    maxPhoneLength: 16,
     vueTelOpts: {
       mode: 'international',
       preferredCountries: ['RU', 'US'],
@@ -102,12 +104,6 @@ export default {
         maxlength: 14,
       },
     },
-    fieldsData: {
-      name: '',
-      phone: '',
-      email: '',
-    },
-    maxPhoneLength: 16,
 
     validPhone: false,
   }),
@@ -122,43 +118,61 @@ export default {
   methods: {
     sendForm() {
       if (this.formProduct) {
-        this.fieldsData.comment = `Клик из формы попапа продукта: ${this.formProduct.name}`;
+        this.fieldsData.comments = `Клик из формы попапа продукта: ${this.formProduct.name}`;
       }
 
-      this.$lander
-        .send(this.fieldsData, {}, this.$route.name === 'edu-platform-slug' ? this.$route.path : undefined)
-        .then(() => {});
+      if (this.checkedValidateError()) {
+        this.$lander
+          .send(
+            this.fieldsData,
+            {},
+            this.$route.name === 'edu-platform-slug' || this.$route.name === 'edu-platform'
+              ? this.$route.path
+              : undefined,
+          )
+          .then(() => {});
+      }
     },
     handlerSave() {
       this.$lander.storage.save('programform', this.fieldsData);
     },
     validatePhone(phone, { valid, number }) {
-      const telOpts = this.vueTelOpts;
-      const inputOpts = telOpts.inputOptions;
-      const isLocalCode = phone[0] === '8';
+      if (phone) {
+        const telOpts = this.vueTelOpts;
+        const inputOpts = telOpts.inputOptions;
+        const isLocalCode = phone[0] === '8';
 
-      inputOpts.maxlength = this.maxPhoneLength;
-      telOpts.autoFormat = !isLocalCode;
+        inputOpts.maxlength = this.maxPhoneLength;
+        telOpts.autoFormat = !isLocalCode;
 
-      this.validPhone = valid && isLocalCode ? phone.length === 11 : valid;
+        this.validPhone = valid && isLocalCode ? phone.length === 11 : valid;
 
-      if (valid) {
-        telOpts.mode = isLocalCode ? 'auto' : 'international';
-        inputOpts.maxlength = isLocalCode ? 11 : number.length;
-      } else {
-        inputOpts.maxlength = 16;
+        if (valid) {
+          telOpts.mode = isLocalCode ? 'auto' : 'international';
+          inputOpts.maxlength = isLocalCode ? 11 : number.length;
+        } else {
+          inputOpts.maxlength = 16;
+        }
+
+        this.validFormData();
       }
-
-      this.validFormData();
     },
     validFormData() {
-      const dataForm = [{ value: this.fieldsData.name }, { value: this.fieldsData.email, type: 'email' }];
+      this.handlerSave();
+      this.checkedValidateError();
+      /* const dataForm = [{ value: this.fieldsData.name }, { value: this.fieldsData.email, type: 'email' }];
       this.validFlag = this.$lander.valid(dataForm) && this.validPhone;
       if (/^([A-ZА-ЯЁ][-,a-z, a-яё. ']+[ ]*)+$/i.test(this.fieldsData.name)) {
         this.validName = true;
       } else {
         this.validName = false;
-      }
+      } */
+    },
+    checkedValidateError() {
+      this.nameErrorFlag = /^([A-ZА-ЯЁ][-,a-z, a-яё. ']+[ ]*)+$/i.test(this.fieldsData.name);
+      this.emailErrorFlag = this.$lander.valid([{ value: this.fieldsData.email, type: 'email' }]);
+      this.phoneErrorFlag = this.validPhone === true && this.fieldsData.phone !== '';
+      return this.nameErrorFlag && this.emailErrorFlag && this.validPhone;
     },
   },
 };
