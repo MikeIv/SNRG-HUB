61df972ba3275470c83b61aa8a393b1148c9db69
Борцов Дмитрий Сергеевич <DBortsov@synergy.ru>
2022-03-02 17:19:10 +0300

    fix(components): скрыт direction.categories
    v-if="false" для блока s-article-detail__directions
    
    НОМЕР ЗАДАЧИ: OGM-178

--- components/s_article_detail/s_article_detail.vue
+++ components/s_article_detail/s_article_detail.vue
@@ -72,7 +72,7 @@
           @click="goToJournal"
         ></a-button>
       </div>
-      <div class="s-article-detail__directions" v-if="categories.length">
+      <div class="s-article-detail__directions" v-if="false">
         <div class="s-article-detail__directions-title a-font_h5">Направления в сфере {{ nameCourse }}</div>
         <div class="s-article-detail__directions-items">
           <nuxt-link :to="`/journal/${category.slug}`" v-for="(category, id) in categories" :key="id">
@@ -103,12 +103,10 @@
 
 <script>
 import './s_article_detail.scss';
-import {
-  MSocialShare, ATag, MCard, AButton,
-} from '@cwespb/synergyui';
+import { MSocialShare, ATag, MCard, AButton } from '@cwespb/synergyui';
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 import AUser from '~/components/_ui/a_user/a_user';
 import MArticle from '~/components/_ui/m_article/m_article';
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 
 export default {
   name: 's-article-detail',
@@ -218,8 +216,8 @@ export default {
       if (
         /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
           navigator.userAgent,
-        )
-        && navigator.share
+        ) &&
+        navigator.share
       ) {
         navigator.share({
           title: this.title,
