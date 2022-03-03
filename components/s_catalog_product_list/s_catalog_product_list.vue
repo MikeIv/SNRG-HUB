ec44777bc017d62b85cf0d8a309316af6f8d2433
Борцов Дмитрий Сергеевич <dbortsov@synergy.ru>
2022-03-03 14:20:37 +0000

    Merge branch 'develop' into 'master'
    feat(components): доработка журнала
    
    See merge request marketplaces/vue/synergymarket!368
    
    (cherry picked from commit 36b4d5bc683200beb446a3da1a637edcce85e782)
    
    be661688 feat(components): made categories component
    3f1f4b2f feat(components): доработка журнала
    b3ddac16 feat(components): redesigned whole s-catalog
    03ae349a feat(components): длительность курса
    d0b4d8ec feat(components): feat(components): доработка журнала
    f135dcd3 feat(components): catalog refactoring
    b4d86fc8 feat(components): блок "Программа обучения" в карточке программ
    bf329e09 feat(components): функционал раскрытия карточки если нет контента
    0e1a21fd Merge branch 'OGM-1872' into 'develop'
    f949fc89 feat(components): continue refactoring catalogs
    f0068aad Merge branch 'OGM-1893' into 'develop'
    d2095325 feat(components): доработка журнала
    6ed2fb68 feat(components): блок Преподаватели и спикеры
    b00c78e6 Merge branch 'OGM-2064' into 'develop'
    1043adb7 feat(components): made journal refactoring
    c5a6dc42 feat(components): made catalogs refactoring
    210ac483 fix(styles): fix catalog styles
    48b9a868 feat(components): made some fixes
    5655c0bf fix(components): fix bug
    dc16a744 fix(components): fix bug
    61df972b fix(components): скрыт direction.categories
    b0839ec0 Merge branch 'OGM-1768' into 'develop'
    8b8fe798 Merge branch 'OGM-1651' into 'develop'

--- components/s_catalog_product_list/s_catalog_product_list.vue
+++ components/s_catalog_product_list/s_catalog_product_list.vue
@@ -3,27 +3,46 @@
     <div class="catalog-product-list__wrapper" v-if="productList">
       <slot />
       <h3 class="catalog-product-list__wrapper-sorry" v-if="!productList.length">К сожалению, ничего нет</h3>
-      <nuxt-link
-        v-for="product in productList"
-        :to="`/product/${product.slug}`"
-        :key="product.id"
-        :class="
-          $route.name === 'organization-slug'
-            ? 'catalog-product-list__item-wrapper-section'
-            : 'catalog-product-list__item-wrapper'
-        "
-      >
-        <m-card
-          type="program"
-          class="catalog-product-list__item"
-          :description="product.included.levels[0].name"
-          :title="product.name"
-          :verticalImgSrc="`${baseURL}/${product.preview_image}`"
-          :bottomText="product.included.organization.abbreviation_name"
-          :iconSrc="`${baseURL}${product.included.organization.logo}`"
-          @organization-click="onOrganizationClick(product)"
-        />
-      </nuxt-link>
+      <template v-if="type === 'journal'">
+        <div v-for="article in productList" :key="article.id" class="catalog-product-list__item-wrapper">
+          <nuxt-link :to="`/article/${article.slug}`">
+            <m-article
+              :article="article"
+              @click.stop="onArticleClick(article)"
+              :randomColor="getRandomColor(article.included.tags[0].name)"
+            />
+          </nuxt-link>
+        </div>
+      </template>
+      <template v-else>
+        <nuxt-link
+          v-for="product in productList"
+          :to="`/${type === 'organizations' ? 'organization' : 'product'}/${product.slug}`"
+          :key="product.id"
+          :class="
+            $route.name === 'organization-slug'
+              ? 'catalog-product-list__item-wrapper-section'
+              : 'catalog-product-list__item-wrapper'
+          "
+        >
+          <m-card
+            type="program"
+            class="catalog-product-list__item"
+            :description="
+              type === 'organizations'
+                ? getProductDescription(product.budget_points, product.contract_points)
+                : product.included.levels[0].name
+            "
+            :title="product.name"
+            :verticalImgSrc="`${baseURL}/${type === 'organizations' ? product.digital_image : product.preview_image}`"
+            :bottomText="
+              type === 'organizations' ? product.abbreviation_name : product.included.organization.abbreviation_name
+            "
+            :iconSrc="`${baseURL}${type === 'organizations' ? product.logo : product.included.organization.logo}`"
+            @organization-click="onOrganizationClick(product)"
+          />
+        </nuxt-link>
+      </template>
     </div>
     <m-pagination
       v-if="productList"
@@ -40,13 +59,15 @@
 <script>
 import { MCard, MPagination } from '@cwespb/synergyui';
 import './s_catalog_product_list.scss';
+import MArticle from '~/components/_ui/m_article/m_article';
 
 export default {
   name: 'SCatalogProductList',
 
-  props: ['productList', 'totalProducts', 'page', 'productsPerPage'],
+  props: ['productList', 'totalProducts', 'page', 'productsPerPage', 'type'],
 
   components: {
+    MArticle,
     MCard,
     MPagination,
   },
@@ -54,6 +75,16 @@ export default {
   data() {
     return {
       baseURL: process.env.NUXT_ENV_S3BACKET,
+      countColorIndex: 0,
+      tagColors: [
+        'rgba(228, 43, 43, 1)',
+        'rgba(5, 161, 143, 1)',
+        'rgba(220, 3, 198, 1)',
+        'rgba(0, 169, 227, 1)',
+        'rgba(67, 86, 255, 1)',
+        'rgba(107, 190, 0, 1)',
+      ],
+      tagColorsList: {},
     };
   },
 
@@ -74,6 +105,31 @@ export default {
   },
 
   methods: {
+    onArticleClick(article) {
+      this.$router.push(`/article/${article.slug}`);
+    },
+
+    getRandomColor(name) {
+      if (Object.prototype.hasOwnProperty.call(this.tagColorsList, name)) {
+        return this.tagColorsList[name];
+      }
+
+      this.tagColorsList[name] = this.tagColors[this.countColorIndex];
+      if (this.countColorIndex === 5) {
+        this.countColorIndex = 0;
+      } else {
+        this.countColorIndex += 1;
+      }
+
+      return this.tagColorsList[name];
+    },
+
+    getProductDescription(budget, contract) {
+      const budgetStr = budget ? `Бюджет: от ${budget} баллов ${contract ? ' • ' : ''}` : '';
+      const contractStr = contract ? `Платное: от ${contract} баллов` : '';
+      return budgetStr + contractStr;
+    },
+
     changeCurrentPage(page) {
       this.pageInx = page;
     },
@@ -87,7 +143,9 @@ export default {
     },
 
     onOrganizationClick(product) {
-      this.$router.push(`/organization/${product.included.organization.slug}`);
+      this.$router.push(
+        `/organization/${this.type === 'organizations' ? product.slug : product.included.organization.slug}`,
+      );
     },
   },
 };
