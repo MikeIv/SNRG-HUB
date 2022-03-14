68e57f244ff5b03a51a0c99d901d3c5b13fec587
evstrv <Tdcnhfnjd9930_>
2022-03-14 20:17:37 +0300

    feat: отправка формы на лендер

--- components/s_banner_top/s_banner_top.vue
+++ components/s_banner_top/s_banner_top.vue
@@ -22,14 +22,12 @@
           <div class="s-banner-top__popup-row">
             <a-input
               class="s-banner-top__popup-input"
-              :class="{ 'error-name': !nameErrorFlag }"
               @input="validFormData"
               v-model="fieldsData.name"
               placeholder="Имя"
             />
             <vue-tel-input
               class="s-banner-top__popup-input"
-              :class="{ error: !phoneErrorFlag }"
               v-bind="vueTelOpts"
               type="phone"
               placeholder="Телефон"
@@ -39,7 +37,6 @@
             </vue-tel-input>
             <a-input
               class="s-banner-top__popup-input"
-              :class="{ 'error-mail': !emailErrorFlag }"
               @input="validFormData"
               v-model="fieldsData.email"
               placeholder="Почта"
@@ -48,16 +45,16 @@
           <div class="s-banner-top__popup-row">
             <a-input
               class="s-banner-top__popup-input institution"
-              :class="{ 'error-institution': !institutionErrorFlag }"
               @input="validFormData"
               v-model="fieldsData.institution"
               placeholder="Название учебного заведения"
             />
             <a-button
               class="s-banner-top__button popup"
+              :disabled="!isChecked || !validFlag"
               bgColor="accent"
               label="Отправить"
-              @click="showAddRequestPopupHandler"
+              @click="sendRequestHandler"
             />
           </div>
         </div>
@@ -77,7 +74,9 @@
 
 <script>
 import { VueTelInput } from 'vue-tel-input';
-import { AButton, APopup, AControl, AInput } from '@cwespb/synergyui';
+import {
+  AButton, APopup, AControl, AInput,
+} from '@cwespb/synergyui';
 import './s_banner_top.scss';
 
 export default {
@@ -94,8 +93,9 @@ export default {
   data() {
     return {
       addRequestPopup: false,
-      isChecked: false,
+      isChecked: true,
 
+      maxPhoneLength: 16,
       vueTelOpts: {
         mode: 'international',
         preferredCountries: ['RU', 'US'],
@@ -111,32 +111,66 @@ export default {
         },
       },
 
-      nameErrorFlag: true,
-      emailErrorFlag: true,
-      phoneErrorFlag: true,
-      institutionErrorFlag: true,
-
       fieldsData: {
         name: '',
         phone: '',
         email: '',
         institution: '',
       },
+      comments: {},
+      land: {},
+
+      validFlag: false,
+      validPhone: false,
     };
   },
 
   methods: {
-    validFormData() {},
+    validFormData() {
+      const dataForm = [
+        { value: this.fieldsData.name },
+        { value: this.fieldsData.email, type: 'email' },
+        { value: this.fieldsData.institution },
+      ];
+      this.validFlag = this.$lander.valid(dataForm) && this.validPhone;
+    },
 
-    validatePhone() {},
+    validatePhone(phone, { valid, number }) {
+      if (phone) {
+        const telOpts = this.vueTelOpts;
+        const inputOpts = telOpts.inputOptions;
+        const isLocalCode = phone[0] === '8';
+
+        inputOpts.maxlength = this.maxPhoneLength;
+        telOpts.autoFormat = !isLocalCode;
+
+        this.validPhone = valid && isLocalCode ? phone.length === 11 : valid;
+
+        if (valid) {
+          telOpts.mode = isLocalCode ? 'auto' : 'international';
+          inputOpts.maxlength = isLocalCode ? 11 : number.length;
+        } else {
+          inputOpts.maxlength = 16;
+        }
+
+        this.validFormData();
+      }
+    },
 
     showAddRequestPopupHandler() {
       this.addRequestPopup = true;
-      console.log('popup', this.addRequestPopup);
     },
 
     sendRequestHandler() {
-      console.log('sendRequestHandler');
+      this.comments = {
+        'comments[Учебное заведение]': this.fieldsData.institution,
+      };
+
+      this.land = {
+        land: 'BANNER.HUB_market',
+      };
+
+      this.$lander.send({ ...this.fieldsData, ...this.comments, ...this.land });
     },
   },
 };
