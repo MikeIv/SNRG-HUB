462d2c66b40c2cad5199f37577a1528cffa8820b
evstrv <Tdcnhfnjd9930_>
2022-03-21 17:10:19 +0300

    fix: fix

--- components/s_partners/s_partners.vue
+++ components/s_partners/s_partners.vue
@@ -4,17 +4,33 @@
       <div class="s-partners__box">
         <h2 class="s-partners__title main" v-html="title"></h2>
         <div class="s-partners__swiper" :key="key">
-          <swiper class="noSwipingClass" ref="partnersSwiper1" :options="swiperOptionsA">
+          <swiper
+            v-if="chunkedList[0] && chunkedList[0].length >= 7"
+            class="noSwipingClass"
+            ref="partnersSwiper1"
+            :options="swiperOptions"
+          >
             <swiper-slide v-for="(company, idx) in chunkedList[0]" :key="idx" class="s-partners__slide">
               <a-logo type="standart" :link="`${baseUrl}${company.logo_image.value}`" />
             </swiper-slide>
           </swiper>
-          <swiper class="noSwipingClass" ref="partnersSwiper2" :options="swiperOptionsB" dir="rtl">
+          <swiper
+            v-if="chunkedList[1] && chunkedList[1].length >= 7"
+            class="noSwipingClass"
+            ref="partnersSwiper2"
+            :options="swiperOptions"
+            dir="rtl"
+          >
             <swiper-slide v-for="(company, idx) in chunkedList[1]" :key="idx" class="s-partners__slide">
               <a-logo type="standart" :link="`${baseUrl}${company.logo_image.value}`" />
             </swiper-slide>
           </swiper>
-          <swiper class="noSwipingClass" ref="partnersSwiper3" :options="swiperOptionsA">
+          <swiper
+            v-if="chunkedList[2] && chunkedList[2].length >= 7"
+            class="noSwipingClass"
+            ref="partnersSwiper3"
+            :options="swiperOptions"
+          >
             <swiper-slide v-for="(company, idx) in chunkedList[2]" :key="idx" class="s-partners__slide">
               <a-logo type="standart" :link="`${baseUrl}${company.logo_image.value}`" />
             </swiper-slide>
@@ -22,16 +38,16 @@
         </div>
       </div>
     </div>
-    <div v-else-if="this.$route.name === 'index' && companyList.length > 21" class="l-wide">
-      <div class="s-partners__box">
-        <h2 class="s-partners__title main" v-html="title"></h2>
-        <div class="s-partners__items main">
-          <div class="s-partners__item" v-for="(company, idx) in companyList" :key="idx">
-            <a-logo type="bordered" :link="`${baseUrl}${company.logo_image.value}`" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <!--    <div v-else-if="this.$route.name === 'index' && companyList.length > 21" class="l-wide">-->
+    <!--      <div class="s-partners__box">-->
+    <!--        <h2 class="s-partners__title main" v-html="title"></h2>-->
+    <!--        <div class="s-partners__items main">-->
+    <!--          <div class="s-partners__item" v-for="(company, idx) in companyList" :key="idx">-->
+    <!--            <a-logo type="bordered" :link="`${baseUrl}${company.logo_image.value}`" />-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
     <div v-else class="s-partners__wrapper">
       <h2 class="s-partners__title s-program-timeline__title other a-font_h2" v-html="title"></h2>
       <div class="s-partners__items">
@@ -67,7 +83,7 @@ export default {
       chunks: 7,
       chunkedList: [],
       baseUrl: process.env.NUXT_ENV_S3BACKET,
-      swiperOptionsA: {
+      swiperOptions: {
         spaceBetween: 12,
         autoHeight: false,
         loop: true,
@@ -83,23 +99,23 @@ export default {
           disableOnInteraction: false,
         },
       },
-      swiperOptionsB: {
-        spaceBetween: 12,
-        autoHeight: false,
-        loop: true,
-        slidesPerView: 'auto',
-        speed: 6000,
-        grabCursor: false,
-        keyboard: false,
-        mousewheel: false,
-        noSwiping: true,
-        noSwipingClass: 'noSwipingClass',
-        autoplay: {
-          delay: 1,
-          // reverseDirection: true,
-          disableOnInteraction: false,
-        },
-      },
+      // swiperOptionsB: {
+      //   spaceBetween: 12,
+      //   autoHeight: false,
+      //   loop: true,
+      //   slidesPerView: 'auto',
+      //   speed: 6000,
+      //   grabCursor: false,
+      //   keyboard: false,
+      //   mousewheel: false,
+      //   noSwiping: true,
+      //   noSwipingClass: 'noSwipingClass',
+      //   autoplay: {
+      //     delay: 1,
+      //     // reverseDirection: true,
+      //     disableOnInteraction: false,
+      //   },
+      // },
       key: 1000,
     };
   },
@@ -134,7 +150,7 @@ export default {
     const expandedMethod = this.methods[0].data;
     const preData = await getEntitiesSectionsDetail(expandedMethod);
     this.companyList = preData.json.items.data;
-    if (this.companyList.length <= 21) {
+    if (this.companyList.length > 0) {
       this.chunkedList.push(this.companyList.slice(0, 7));
       this.chunkedList.push(this.companyList.slice(7, 14));
       this.chunkedList.push(this.companyList.slice(14));
@@ -142,7 +158,7 @@ export default {
     // console.log('this.chunkedList', this.chunkedList);
     // this.swiperCount = Math.ceil(this.companyList.length / this.chunks);
     // this.chunkArray(this.companyList, this.chunks);
-    this.key += 1;
+    // this.key += 1;
   },
 };
 </script>
