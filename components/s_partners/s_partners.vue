237825289b0586caa0d161d1f4bca6f0847042fd
evstrv <Tdcnhfnjd9930_>
2022-03-21 17:36:23 +0300

    feat: убрал коменты

--- components/s_partners/s_partners.vue
+++ components/s_partners/s_partners.vue
@@ -38,16 +38,6 @@
         </div>
       </div>
     </div>
-    <!--    <div v-else-if="this.$route.name === 'index' && companyList.length > 21" class="l-wide">-->
-    <!--      <div class="s-partners__box">-->
-    <!--        <h2 class="s-partners__title main" v-html="title"></h2>-->
-    <!--        <div class="s-partners__items main">-->
-    <!--          <div class="s-partners__item" v-for="(company, idx) in companyList" :key="idx">-->
-    <!--            <a-logo type="bordered" :link="`${baseUrl}${company.logo_image.value}`" />-->
-    <!--          </div>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--    </div>-->
     <div v-else class="s-partners__wrapper">
       <h2 class="s-partners__title s-program-timeline__title other a-font_h2" v-html="title"></h2>
       <div class="s-partners__items">
@@ -99,52 +89,11 @@ export default {
           disableOnInteraction: false,
         },
       },
-      // swiperOptionsB: {
-      //   spaceBetween: 12,
-      //   autoHeight: false,
-      //   loop: true,
-      //   slidesPerView: 'auto',
-      //   speed: 6000,
-      //   grabCursor: false,
-      //   keyboard: false,
-      //   mousewheel: false,
-      //   noSwiping: true,
-      //   noSwipingClass: 'noSwipingClass',
-      //   autoplay: {
-      //     delay: 1,
-      //     // reverseDirection: true,
-      //     disableOnInteraction: false,
-      //   },
-      // },
       key: 1000,
     };
   },
 
   props: ['methods', 'title'],
-
-  // methods: {
-  //   chunkArray(array, chunk) {
-  //     for (let i = 0; i < array.length; i += chunk) {
-  //       const newChunk = array.slice(i, i + chunk);
-  //       if (array.slice(i, i + chunk).length < 7 && array.slice(i, i + chunk).length > 3) {
-  //         this.chunkedList.push([...newChunk, ...newChunk]);
-  //       } else if (array.slice(i, i + chunk).length <= 3) {
-  //         // eslint-disable-next-line max-len
-  //         this.chunkedList.push([
-  //           ...newChunk,
-  //           ...newChunk,
-  //           ...newChunk,
-  //           ...newChunk,
-  //           ...newChunk,
-  //           ...newChunk,
-  //           ...newChunk,
-  //         ]);
-  //       } else {
-  //         this.chunkedList.push(newChunk);
-  //       }
-  //     }
-  //   },
-  // },
 
   async mounted() {
     const expandedMethod = this.methods[0].data;
@@ -155,10 +104,6 @@ export default {
       this.chunkedList.push(this.companyList.slice(7, 14));
       this.chunkedList.push(this.companyList.slice(14));
     }
-    // console.log('this.chunkedList', this.chunkedList);
-    // this.swiperCount = Math.ceil(this.companyList.length / this.chunks);
-    // this.chunkArray(this.companyList, this.chunks);
-    // this.key += 1;
   },
 };
 </script>
