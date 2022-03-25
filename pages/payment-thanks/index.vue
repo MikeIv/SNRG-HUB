833728195fa377c6e920005113dfa8a02f297d99
VEvstratov <Tdcnhfnjd22>
2022-03-25 07:48:15 +0300

    feat: добавил страницу спасибо (шаблон)

---
+++ pages/payment-thanks/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="thanks__wrapper">
+    <SHeader @search="search = $event" />
+    <SProductSearch v-if="search" :search="search" />
+    <PaymentThanks v-else />
+    <SFooter />
+    <Popups />
+  </div>
+</template>
+
+<script>
+import PaymentThanks from '~/components/payment_thanks/payment_thanks';
+import SHeader from '~/components/s_header/s_header';
+import SFooter from '~/components/s_footer/s_footer';
+import SProductSearch from '~/components/s_product_search/s_product_search';
+import Popups from '~/components/popups/popups';
+
+export default {
+  layout: 'empty',
+  components: {
+    PaymentThanks,
+    SHeader,
+    SFooter,
+    SProductSearch,
+    Popups,
+  },
+
+  data() {
+    return {
+      search: '',
+    };
+  },
+};
+</script>
