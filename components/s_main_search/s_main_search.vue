62f6cfceb2462e060b8333fa125f98df4846a041
evstrv <Tdcnhfnjd9930_>
2022-04-08 16:23:34 +0300

    feat: ошибка в слове

--- components/s_main_search/s_main_search.vue
+++ components/s_main_search/s_main_search.vue
@@ -81,7 +81,7 @@ export default {
     return {
       rows: [
         {
-          name: 'Выберете направление',
+          name: 'Выберите направление',
           key: 'category',
           items: [],
         },
