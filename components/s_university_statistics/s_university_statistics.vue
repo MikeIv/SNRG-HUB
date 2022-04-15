4a4c19d502d3e55f3091aad7bb4c1f52e021d082
ogairodion <ogairodion@gmail.com>
2022-04-15 15:29:16 +0300

    fix(components): правка верстки блоков
    НОМЕР ЗАДАЧИ: OGM-2512

--- components/s_university_statistics/s_university_statistics.vue
+++ components/s_university_statistics/s_university_statistics.vue
@@ -1,19 +1,21 @@
 <template>
-  <section class="s-university-statistics">
-    <div class="s-university-statistics__header">
-      <h2 class="s-university-statistics__title a-font_h2" v-html="title"></h2>
-      <div class="s-university-statistics__tags">
-        <span v-for="tag in tags" :key="tag.id" class="s-university-statistics__tag"
-          ><i class="si-done"></i> {{ tag }}</span
-        >
+  <section class="s-university-statistics s-margin">
+    <div class="l-wide l-border-radius">
+      <div class="s-university-statistics__header">
+        <h2 class="s-university-statistics__title a-font_h2" v-html="title"></h2>
+        <div class="s-university-statistics__tags">
+          <span v-for="tag in tags" :key="tag.id" class="s-university-statistics__tag"
+            ><i class="si-done"></i> {{ tag }}</span
+          >
+        </div>
       </div>
-    </div>
 
-    <swiper ref="awesomeSwiper" :options="swiperOptionStatistics" class="s-university-statistics__factoids">
-      <swiper-slide v-for="factoid in factoids" :key="factoid.id" class="">
-        <a-factoid :color="factoid.color" :type="factoid.type" :title="factoid.title" :number="factoid.number" />
-      </swiper-slide>
-    </swiper>
+      <swiper ref="awesomeSwiper" :options="swiperOptionStatistics" class="s-university-statistics__factoids">
+        <swiper-slide v-for="factoid in factoids" :key="factoid.id" class="">
+          <a-factoid :color="factoid.color" :type="factoid.type" :title="factoid.title" :number="factoid.number" />
+        </swiper-slide>
+      </swiper>
+    </div>
   </section>
 </template>
 
@@ -79,7 +81,7 @@ export default {
             slidesPerView: 3,
             spaceBetween: 8,
           },
-          768: {
+          1199: {
             slidesPerView: 4,
             spaceBetween: 20,
           },
