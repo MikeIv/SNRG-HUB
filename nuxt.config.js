5c8f7eba40d95daf7d20466a750ce9a633d322a9
evstrv <Tdcnhfnjd9930_>
2022-05-11 16:37:11 +0300

    feat: process.env.NODE_ENV

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,8 +96,8 @@ export default {
     ['@synergy/synergyid-adapter', {
       namespace: 'auth',
       brokerBackendUrl: 'https://api.synergyhub.ru/',
-      synergyIdBackendUrl: process.env.NODE_ENV === 'development' ? 'https://ogm-111-2795.c4.syndev.ru/' : 'https://pass.synergy.ru/',
-      synergyIdDomain: process.env.NODE_ENV === 'development' ? 'https://ogm-002-2640.c4.syndev.ru/' : 'https://pass.synergy.ru/',
+      synergyIdBackendUrl: process.env.IS_PRODUCTION === 'development' ? 'https://ogm-111-2795.c4.syndev.ru/' : 'https://pass.synergy.ru/',
+      synergyIdDomain: process.env.IS_PRODUCTION === 'development' ? 'https://ogm-002-2640.c4.syndev.ru/' : 'https://pass.synergy.ru/',
     },
     ],
   ],
@@ -119,6 +119,7 @@ export default {
     SITE_FB_PIXEL: process.env.SITE_FB_PIXEL,
     SITE_GTM: process.env.SITE_GTM,
     DADATA_KEY: process.env.DADATA_KEY,
+    IS_PRODUCTION: process.env.NODE_ENV,
   },
 
   publicRuntimeConfig: {
