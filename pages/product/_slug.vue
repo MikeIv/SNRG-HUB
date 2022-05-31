3c14a7c6cb4b1fb0a65fef451316ca1dcba68090
Roman Belov <roman.belov.94@gmail.com>
2022-05-31 17:10:02 +0300

    feat: 🎸 add comp
    add comp

--- pages/product/_slug.vue
+++ pages/product/_slug.vue
@@ -4,6 +4,7 @@
       <component :is="key" :methods="methods" :title="title" :productIds="pageInfo.entity_page"></component>
     </LazyHydrate> -->
     <s-program-skills />
+    <s-program-minimum-score />
     <!-- <SProgramPrice /> -->
   </div>
 </template>
@@ -12,10 +13,13 @@
 /* import LazyHydrate from 'vue-lazy-hydration'; */
 /* import SProgramPrice from '~/components/s_program_price/s_program_price'; */
 
+import SProgramMinimumScore from '~/components/product/s_program_minimum_score/s_program_minimum_score';
+
 export default {
   layout: 'product',
 
   components: {
+    SProgramMinimumScore,
     /* LazyHydrate, */
     /* SProgramPrice, */
   },
