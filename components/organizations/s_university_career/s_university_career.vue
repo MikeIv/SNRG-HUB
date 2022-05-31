0f0851f924a89ac7436ed0d13941c84094755cb4
NMumiatov <NMumiatov@synergy.ru>
2022-05-31 18:20:08 +0300

    feat(components): s_unuversity_career
    Перенос и настройка секции
    
    НОМЕР ЗАДАЧИ: OGM-3030

---
+++ components/organizations/s_university_career/s_university_career.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="s-university-career s-margin">
+    <div class="l-wide l-border-radius">
+      <div class="s-university-career__wrap">
+        <h2 class="s-university-career__title a-font_h2" v-html="sectionData.title"></h2>
+        <div class="s-university-career__items">
+          <AFactoids
+            v-for="(factoid, index) in sectionData.items"
+            :key="index"
+            :type="factoid.type"
+            :title="factoid.title"
+            :lineNumber="factoid.lineNumber"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AFactoids from '@/components/_ui/A-factoids/A-factoids';
+import './s_university_career.scss';
+import getOrganizationSectionInfo from '~/api/organizationSectionInfo';
+
+export default {
+  name: 'SUniversityCareer',
+
+  components: {
+    AFactoids,
+  },
+
+  data() {
+    return {
+      sectionData: null,
+    };
+  },
+
+  async mounted() {
+    const requestData = { slug: this.$route.params.slug, key: 's-university-career' };
+    this.sectionData = await getOrganizationSectionInfo(requestData);
+  },
+};
+</script>
