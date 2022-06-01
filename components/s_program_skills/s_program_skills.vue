c9065df3c0e51f345d4194c899ef105bca69e4af
AAvtushenko <AAvtushenko@synergy.ru>
2022-06-01 17:06:08 +0300

    feat(components): refactor several blocks and catalog_section

--- components/s_program_skills/s_program_skills.vue
+++ components/s_program_skills/s_program_skills.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="s-program-skills s-margin">
+  <section v-if="sectionData" class="s-program-skills s-margin">
     <div class="l-wide l-border-radius">
-      <h2 class="s-program-skills__title a-font_h2" v-html="sectionData.title"></h2>
+      <h2 class="s-program-skills__title a-font_h2" v-html="sectionData.title" />
       <div class="s-program-skills__items">
         <AListElement v-for="(item, index) in sectionData.items" :key="index" :type="item.type" :label="item.value" />
       </div>
@@ -27,7 +27,7 @@ export default {
     };
   },
 
-  async mounted() {
+  async fetch() {
     const requestData = { slug: this.$route.params.slug, key: 's-program-skills' };
     this.sectionData = await productSectionInfo(requestData);
   },
