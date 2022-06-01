42d79ebbc6f05276dab0fb39262a2eb0d72bb62c
Белов Роман Владиславович <RBelov@synergy.ru>
2022-06-01 12:58:24 +0300

    feat(components): feat(components): feat(components): fix styles
    fix styles
    НОМЕР ЗАДАЧИ: OGM-3166

--- components/product/s_program_infoblock/s_program_infoblock.vue
+++ components/product/s_program_infoblock/s_program_infoblock.vue
@@ -1,21 +1,23 @@
 <template>
-  <section class="s-program-infoblock l-wide l-border-radius s-margin">
-    <div class="s-program-infoblock__wrapper">
-      <div class="s-program-infoblock__items">
-        <swiper :options="swiperOptionProgramInfo">
-          <swiper-slide v-for="item in sectionData.items" :key="item.id" class="s-program-infoblock__item">
-            <AFactoids :type="item.type" :title="item.title" :lineNumber="item.lineNumber" class="swiper-slide" />
-          </swiper-slide>
-        </swiper>
+  <section class="s-program-infoblock s-margin">
+    <div class="l-wide l-border-radius">
+      <div class="s-program-infoblock__wrapper">
+        <div class="s-program-infoblock__items">
+          <swiper :options="swiperOptionProgramInfo">
+            <swiper-slide v-for="item in sectionData.items" :key="item.id" class="s-program-infoblock__item">
+              <AFactoids :type="item.type" :title="item.title" :lineNumber="item.lineNumber" class="swiper-slide" />
+            </swiper-slide>
+          </swiper>
+        </div>
+        <span class="s-program-infoblock__arrow">
+          <svg viewBox="0 0 259 482" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M259 482L240.794 353.75L180.22 297.382L119.645 241.014L240.794 128.28L259 0L0 240.987L259 482Z"
+              fill="#FF0040"
+            ></path>
+          </svg>
+        </span>
       </div>
-      <span class="s-program-infoblock__arrow">
-        <svg viewBox="0 0 259 482" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M259 482L240.794 353.75L180.22 297.382L119.645 241.014L240.794 128.28L259 0L0 240.987L259 482Z"
-            fill="#FF0040"
-          ></path>
-        </svg>
-      </span>
     </div>
   </section>
 </template>
