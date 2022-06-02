aad6427abeab017343641dbaa46f785489dfd202
Белов Роман Владиславович <RBelov@synergy.ru>
2022-06-02 17:00:22 +0300

    feat(components): add block s-program-content
    add block s-program-content
    
    НОМЕР ЗАДАЧИ: OGM-3167

--- components/product/s_program_content/s_program_content.vue
+++ components/product/s_program_content/s_program_content.vue
@@ -6,7 +6,7 @@
           <h2 class="s-program-content__title a-font_h2" v-html="sectionData.title"></h2>
           <div class="s-program-content__numbs">
             <AFactoids
-              v-for="factoid in sectionData.rightItems"
+              v-for="factoid in sectionData.programContentRightItems"
               :key="factoid.id"
               :type="factoid.type"
               :title="factoid.title"
@@ -17,7 +17,12 @@
         </div>
 
         <div class="s-program-content__body">
-          <div class="s-program-content__row" v-for="item in sectionData.items" :key="item.id" @click="showMore(item)">
+          <div
+            class="s-program-content__row"
+            v-for="item in sectionData.programContentList"
+            :key="item.id"
+            @click="showMore(item)"
+          >
             <div class="s-program-content__head">
               <div class="title a-font_xxl">{{ item.title }}</div>
               <div
@@ -49,7 +54,7 @@
 import { AListElement } from '@cwespb/synergyui';
 import AFactoids from '@/components/_ui/A-factoids/A-factoids';
 import './s_program_content.scss';
-// import productSectionInfo from '~/api/productSectionInfo';
+import productSectionInfo from '~/api/productSectionInfo';
 
 export default {
   name: 'SProgramContent',
@@ -61,126 +66,7 @@ export default {
 
   data() {
     return {
-      sectionData: {
-        title: 'Программа обучения',
-        items: [
-          {
-            id: 1,
-            title: 'Базовый курс по PHP',
-            isActive: true,
-            listItems: [
-              {
-                id: 1,
-                type: 'numbered',
-                // eslint-disable-next-line operator-linebreak, max-len
-                text: 'Что такое PHP. Установка PHP. Редакторы для работы с кодомЧто такое PHP. Установка PHP. Редакторы для работы с кодомЧто такое PHP. Установка PHP. Редакторы для работы с кодом',
-                number: 1,
-              },
-              {
-                id: 2,
-                type: 'numbered',
-                text: 'Константы, операторы сравнения, ветвления',
-                number: 2,
-              },
-              {
-                id: 3,
-                type: 'numbered',
-                text: 'Массивы, отслеживание потребления памяти',
-                number: 3,
-              },
-              {
-                id: 4,
-                type: 'numbered',
-                // eslint-disable-next-line operator-linebreak, max-len
-                text: 'Консольные скрипты, интерактивная консоль, настройка php, потоки ввода-вывода, аргументы запуска скриптов',
-                number: 4,
-              },
-            ],
-          },
-          {
-            id: 2,
-            title: 'Продвинутый курс PHP',
-            isActive: false,
-            listItems: [
-              {
-                id: 1,
-                type: 'numbered',
-                // eslint-disable-next-line operator-linebreak, max-len
-                text: 'Что такое PHP. Установка PHP. Редакторы для работы с кодомЧто такое PHP. Установка PHP. Редакторы для работы с кодомЧто такое PHP. Установка PHP. Редакторы для работы с кодом',
-                number: 1,
-              },
-              {
-                id: 2,
-                type: 'numbered',
-                text: 'Константы, операторы сравнения, ветвления',
-                number: 2,
-              },
-              {
-                id: 3,
-                type: 'numbered',
-                text: 'Массивы, отслеживание потребления памяти',
-                number: 3,
-              },
-              {
-                id: 4,
-                type: 'numbered',
-                // eslint-disable-next-line operator-linebreak, max-len
-                text: 'Консольные скрипты, интерактивная консоль, настройка php, потоки ввода-вывода, аргументы запуска скриптов',
-                number: 4,
-              },
-            ],
-          },
-          {
-            id: 3,
-            title: 'GIT и командная работа',
-            isActive: false,
-            listItems: [
-              {
-                id: 1,
-                type: 'numbered',
-                // eslint-disable-next-line operator-linebreak, max-len
-                text: 'Что такое PHP. Установка PHP. Редакторы для работы с кодомЧто такое PHP. Установка PHP. Редакторы для работы с кодомЧто такое PHP. Установка PHP. Редакторы для работы с кодом',
-                number: 1,
-              },
-              {
-                id: 2,
-                type: 'numbered',
-                text: 'Константы, операторы сравнения, ветвления',
-                number: 2,
-              },
-              {
-                id: 3,
-                type: 'numbered',
-                text: 'Массивы, отслеживание потребления памяти',
-                number: 3,
-              },
-              {
-                id: 4,
-                type: 'numbered',
-                // eslint-disable-next-line operator-linebreak, max-len
-                text: 'Консольные скрипты, интерактивная консоль, настройка php, потоки ввода-вывода, аргументы запуска скриптов',
-                number: 4,
-              },
-            ],
-          },
-        ],
-        rightItems: [
-          {
-            id: 1,
-            title: 'тематических модуля',
-            number: 93,
-            type: 'number',
-            color: 'color_link',
-          },
-          {
-            id: 2,
-            title: 'онлайн-уроков',
-            number: 280,
-            type: 'number',
-            color: 'color_link',
-          },
-        ],
-      },
+      sectionData: null,
       programContentList: [],
       programContentRightItems: [],
       direction: 'down',
@@ -190,18 +76,18 @@ export default {
 
   props: ['slug'],
 
-  // async fetch() {
-  //   const requestData = { slug: this.slug || this.$route.params.slug, key: 's-program-skills' };
-  //   this.sectionData = await productSectionInfo(requestData);
-  // },
+  async fetch() {
+    const requestData = { slug: this.slug || this.$route.params.slug, key: 's-program-content' };
+    this.sectionData = await productSectionInfo(requestData);
+  },
 
   methods: {
     showMore(elem) {
-      this.sectionData.items.forEach((item, i) => {
+      this.sectionData.programContentList.forEach((item, i) => {
         if (item === elem) {
-          this.sectionData.items[i].isActive = !this.sectionData.items[i].isActive;
+          this.sectionData.programContentList[i].isActive = !this.sectionData.programContentList[i].isActive;
         } else {
-          this.sectionData.items[i].isActive = false;
+          this.sectionData.programContentList[i].isActive = false;
         }
       });
     },
